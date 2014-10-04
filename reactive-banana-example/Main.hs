668fe01b41341e73356a28348046c6eb149cc26a
Samuel Gélineau <gelisam+github@gmail.com>
2014-10-04 16:15:29 -0400

    make reactive-banana-example match spec

--- reactive-banana-example/Main.hs
+++ reactive-banana-example/Main.hs
@@ -32,9 +32,12 @@ mainBanana :: forall t. Frameworks t
            -> Event t InputEvent
            -> Moment t (Behavior t Picture)
 mainBanana _ glossEvent = do
+    -- used in the dynamic version.
     resetClick0 <- trimE click0
     go resetClick0
   where
+    -- Part 1: static version
+    
     -- Input
     
     click0, click5, click10 :: Event t ()
@@ -56,13 +59,15 @@ mainBanana _ glossEvent = do
     mode10 = accumB True (eachE toggle10 not)
     
     count0, count5, count10 :: Behavior t Int
-    count0 = accumB 0 (eachE click0 (+1))
-    count5 = accumB 0 (whenE mode5 (eachE click5 (+1)))
-    count10 = accumB 0 (eachE click10 (+1))
+    count0  = accumB 0 $ eachE toggle0 (const 0)
+                 `union` eachE click0 (+1)
+    count5  = accumB 0 $ whenE mode5
+                       $ eachE click5 (+1)
+    count10 = accumB 0 $ eachE click10 (+1)
     
     
-    -- Separate code for the graph changes of scenario 0,
-    -- because of scoping constraints.
+    -- Part 2: dynamic version
+    -- (in a separate function due to scoping constraints)
     
     go :: AnyMoment Event () -> Moment t (Behavior t Picture)
     go resetClick0 = return picture
@@ -75,19 +80,25 @@ mainBanana _ glossEvent = do
             recentClick0 <- now resetClick0
             return $ accumB 0 (eachE recentClick0 (+1))
         
-        recentCount0 :: Behavior t Int
-        recentCount0 = switchB count0 (eachE resetRequest newCount0)
+        dynamicCount0 :: Behavior t Int
+        dynamicCount0 = switchB count0 (eachE resetRequest newCount0)
         
         
         -- Output
         
-        output5, output10 :: Behavior t Int
-        output0  = if_then_else <$> mode0  <*> recentCount0 <*> pure (-1)
-        output5  = if_then_else <$> mode5  <*> count5       <*> pure (-1)
-        output10 = if_then_else <$> mode10 <*> count10      <*> pure (-1)
+        minus1 :: Behavior t Int
+        minus1 = pure (-1)
+        
+        output0, dynamicOutput0, output5, output10 :: Behavior t Int
+        output0        = if_then_else <$> mode0  <*> count0        <*> minus1
+        dynamicOutput0 = if_then_else <$> mode0  <*> dynamicCount0 <*> minus1
+        output5        = if_then_else <$> mode5  <*> count5        <*> minus1
+        output10       = if_then_else <$> mode10 <*> count10       <*> minus1
         
         picture :: Behavior t Picture
-        picture = renderButtons <$> output0 <*> output5 <*> output10
+        picture = renderButtons <$> output0  <*> (Just <$> dynamicOutput0)
+                                <*> output5  <*> pure Nothing
+                                <*> output10 <*> pure Nothing
 
 
 -- Gloss event loop
