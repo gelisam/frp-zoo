db0ce7636ddebf22258de1c0516c37c7229a07f3
Samuel Gélineau <gelisam+github@gmail.com>
2014-10-07 08:43:34 -0400

    static implementation for elerea

---
+++ elerea-example/GlossInterface.hs
@@ -0,0 +1,37 @@
+module GlossInterface where
+
+import FRP.Elerea.Clocked
+import Graphics.Gloss (Color, Display, Picture)
+import qualified Graphics.Gloss.Interface.IO.Game as G
+
+
+type InputEvent = G.Event
+
+playElerea
+  :: Display
+  -> Color
+  -> Int
+  -> ( Signal (Maybe Float)
+    -> Signal (Maybe InputEvent)
+    -> SignalGen (Signal Picture))
+  -> IO ()
+playElerea display color frequency network = do
+    (tickSignal, fireTickSignal) <- external Nothing
+    (inputSignal, fireInputSignal) <- external Nothing
+    
+    recomputePicture <- start (network tickSignal inputSignal)
+    initialPicture <- recomputePicture
+    
+    G.playIO display
+             color
+             frequency
+             initialPicture
+             return
+             (\e _ -> do
+               fireTickSignal Nothing
+               fireInputSignal (Just e)
+               recomputePicture)
+             (\e _ -> do
+               fireTickSignal (Just e)
+               fireInputSignal Nothing
+               recomputePicture)
---
+++ elerea-example/Main.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import Control.Applicative
+import FRP.Elerea.Clocked
+import Graphics.Gloss.Interface.IO.Game hiding (Event)
+
+import Buttons
+import GlossInterface
+
+-- Utilities
+
+if_then_else :: Bool -> a -> a -> a
+if_then_else True  t _ = t
+if_then_else False _ f = f
+
+fmapMaybe :: (a -> Maybe b) -> Signal (Maybe a) -> Signal (Maybe b)
+fmapMaybe f = fmap (>>= f)
+
+whenEq :: Eq a => a -> Signal a -> Signal Bool
+whenEq x = fmap (== x)
+
+andAlso :: Signal Bool -> Signal Bool -> Signal Bool
+andAlso = liftA2 (&&)
+
+pair :: Signal a -> Signal b -> Signal (a,b)
+pair = liftA2 (,)
+
+
+-- FRP network
+
+mainElerea :: Signal (Maybe Float)
+           -> Signal (Maybe InputEvent)
+           -> SignalGen (Signal Picture)
+mainElerea _ glossEvent = do
+    -- Part 1: static version
+
+    -- Input
+    
+    let click0  = whenEq (Just Click) $ fmapMaybe filter0  glossEvent
+    let click5  = whenEq (Just Click) $ fmapMaybe filter5  glossEvent
+    let click10 = whenEq (Just Click) $ fmapMaybe filter10 glossEvent
+    
+    let toggle0  = whenEq (Just Toggle) $ fmapMaybe filter0  glossEvent
+    let toggle5  = whenEq (Just Toggle) $ fmapMaybe filter5  glossEvent
+    let toggle10 = whenEq (Just Toggle) $ fmapMaybe filter10 glossEvent
+    
+    
+    -- Behaviour
+    
+    let alternate = stateful True not
+    mode0  <- withClock toggle0  alternate
+    mode5  <- withClock toggle5  alternate
+    mode10 <- withClock toggle10 alternate
+    
+    count0  <- transfer 0 (\case (True, False) -> const 0
+                                 (False, True) -> (+1)
+                                 _             -> id)
+                          (pair toggle0 click0)
+    count5  <- withClock (mode5 `andAlso` click5)
+             $ stateful 0 (+1)
+    count10 <- withClock click10
+             $ stateful 0 (+1)
+    
+    
+    -- Output
+    
+    let minus1 = pure (-1)
+    let output0  = if_then_else <$> mode0  <*> count0  <*> minus1
+    let output5  = if_then_else <$> mode5  <*> count5  <*> minus1
+    let output10 = if_then_else <$> mode10 <*> count10 <*> minus1
+    
+    return $ renderButtons <$> output0  <*> pure Nothing
+                           <*> output5  <*> pure Nothing
+                           <*> output10 <*> pure Nothing
+
+
+-- Gloss event loop
+
+main :: IO ()
+main = playElerea (InWindow "Elerea Example" (320, 240) (800, 200))
+                  white
+                  300
+                  mainElerea
