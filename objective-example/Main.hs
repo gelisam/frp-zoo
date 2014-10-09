96cf70d681c11052f7976333eee7e6260417c270
Fumiaki Kinoshita <fumiexcel@gmail.com>
2014-10-09 22:46:57 +0900

    higher-order

--- objective-example/Main.hs
+++ objective-example/Main.hs
@@ -5,7 +5,7 @@ import Control.Object
 import Control.Monad.Objective
 import Control.Applicative
 import Buttons
-
+import Control.Monad
 import Graphics.Gloss.Interface.IO.Game
 
 data Async a b r where
@@ -21,23 +21,32 @@ bool :: a -> a -> Bool -> a
 bool a _ False = a
 bool _ b True = b
 
-toggler :: Monad m => Object (Async ButtonClick Bool) m
-toggler = \case { Click -> id; Toggle -> not } `foldO` True
-
 count0 :: IO (Address (Async ButtonClick Int) IO)
 count0 = do
   counter <- new $ \case { Click -> (+1); Toggle -> const 0 } `foldO` (0 :: Int)
-  mode <- new toggler
+  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
       counter .- Push b
       mode .- Push b
     Pull -> (mode .- Pull) >>= bool (return (-1)) (counter .- Pull)
 
+count0D :: IO (Address (Async ButtonClick Int) IO)
+count0D = do
+  let counter = \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
+  holder <- new counter >>= new . foldO const
+  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
+  new $ liftO $ \case
+    Push b -> do
+      holder .- Pull >>= (.- Push b)
+      mode .- Push b
+      when (b == Toggle) $ new counter >>= (holder .-) . Push
+    Pull -> (mode .- Pull) >>= bool (return (-1)) (holder .- Pull >>= (.- Pull))
+
 count5 :: IO (Address (Async ButtonClick Int) IO)
 count5 = do
   counter <- new $ \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
-  mode <- new toggler
+  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
       mode .- Push b
@@ -47,7 +56,7 @@ count5 = do
 count10 :: IO (Address (Async ButtonClick Int) IO)
 count10 = do
   counter <- new $ \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
-  mode <- new toggler
+  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
       counter .- Push b
@@ -56,17 +65,19 @@ count10 = do
 
 main = do
   c0 <- count0
+  c0' <- count0D
   c5 <- count5
   c10 <- count10
   playIO (InWindow "Objective Example" (320, 240) (800, 200))
                  white
                  300
                  ()
-                 (const $ renderButtons <$> (c0 .- Pull) <*> pure Nothing
+                 (const $ renderButtons <$> (c0 .- Pull) <*> (fmap Just $ c0' .- Pull)
                   <*> (c5 .- Pull) <*> pure Nothing
                   <*> (c10 .- Pull) <*> pure Nothing)
                  (\e () -> do
                     maybe (return ()) ((c0 .-) . Push) $ filter0 e
+                    maybe (return ()) ((c0' .-) . Push) $ filter0 e
                     maybe (return ()) ((c5 .-) . Push) $ filter5 e
                     maybe (return ()) ((c10 .-) . Push) $ filter10 e)
                  (\_ _ -> return ())
