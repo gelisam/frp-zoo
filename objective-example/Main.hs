d3e9813b9132ff8770fb16fddaec316ef7a34f2b
Fumiaki Kinoshita <fumiexcel@gmail.com>
2015-03-09 17:18:48 +0900

    make objective-example up to date (#14)

--- objective-example/Main.hs
+++ objective-example/Main.hs
@@ -2,7 +2,6 @@
 module Main where
 
 import Control.Object
-import Control.Monad.Objective
 import Control.Applicative
 import Buttons
 import Control.Monad
@@ -21,63 +20,69 @@ bool :: a -> a -> Bool -> a
 bool a _ False = a
 bool _ b True = b
 
-count0 :: IO (Address (Async ButtonClick Int) IO)
+count0 :: IO (Instance (Async ButtonClick Int) IO)
 count0 = do
-  counter <- new $ \case { Click -> (+1); Toggle -> const 0 } `foldO` (0 :: Int)
-  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
+  counter <- newSettle $ \case { Click -> (+1); Toggle -> const 0 } `foldO` (0 :: Int)
+  mode <- newSettle $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
       counter .- Push b
       mode .- Push b
-    Pull -> (mode .- Pull) >>= bool (return (-1)) (counter .- Pull)
+    Pull -> pull mode >>= bool (return (-1)) (pull counter)
 
-count0D :: IO (Address (Async ButtonClick Int) IO)
+count0D :: IO (Instance (Async ButtonClick Int) IO)
 count0D = do
   let counter = \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
-  holder <- new counter >>= new . foldO const
-  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
+  holder <- newSettle counter >>= newSettle . foldO const
+  mode <- newSettle $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
-      holder .- Pull >>= (.- Push b)
+      pull holder >>= (.- Push b)
       mode .- Push b
       when (b == Toggle) $ new counter >>= (holder .-) . Push
-    Pull -> (mode .- Pull) >>= bool (return (-1)) (holder .- Pull >>= (.- Pull))
+    Pull -> pull mode >>= bool (return (-1)) (pull holder >>= pull)
 
-count5 :: IO (Address (Async ButtonClick Int) IO)
+count5 :: IO (Instance (Async ButtonClick Int) IO)
 count5 = do
-  counter <- new $ \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
-  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
+  counter <- newSettle $ \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
+  mode <- newSettle $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
       mode .- Push b
-      (mode .- Pull) >>= bool (return ()) (counter .- Push b)
-    Pull -> (mode .- Pull) >>= bool (return (-1)) (counter .- Pull)
+      pull mode >>= bool (return ()) (counter .- Push b)
+    Pull -> pull mode >>= bool (return (-1)) (pull counter)
 
-count10 :: IO (Address (Async ButtonClick Int) IO)
+count10 :: IO (Instance (Async ButtonClick Int) IO)
 count10 = do
-  counter <- new $ \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
-  mode <- new $ \case { Click -> id; Toggle -> not } `foldO` True
+  counter <- newSettle $ \case { Click -> (+1); Toggle -> id } `foldO` (0 :: Int)
+  mode <- newSettle $ \case { Click -> id; Toggle -> not } `foldO` True
   new $ liftO $ \case
     Push b -> do
       counter .- Push b
       mode .- Push b
-    Pull -> (mode .- Pull) >>= bool (return (-1)) (counter .- Pull)
+    Pull -> pull mode >>= bool (return (-1)) (pull counter)
+
+pull :: Instance (Async a b) IO -> IO b
+pull = (.-Pull)
 
 main = do
   c0 <- count0
   c0' <- count0D
   c5 <- count5
   c10 <- count10
+  let push i (Just a) = i .- Push a
+      push _ _ = return ()
+
   playIO (InWindow "Objective Example" (320, 240) (800, 200))
-                 white
-                 30
-                 ()
-                 (const $ renderButtons <$> (c0 .- Pull) <*> (fmap Just $ c0' .- Pull)
-                  <*> (c5 .- Pull) <*> pure Nothing
-                  <*> (c10 .- Pull) <*> pure Nothing)
-                 (\e () -> do
-                    maybe (return ()) ((c0 .-) . Push) $ filter0 e
-                    maybe (return ()) ((c0' .-) . Push) $ filter0 e
-                    maybe (return ()) ((c5 .-) . Push) $ filter5 e
-                    maybe (return ()) ((c10 .-) . Push) $ filter10 e)
-                 (\_ _ -> return ())
+           white
+           30
+           ()
+           (const $ renderButtons <$> (c0 .- Pull) <*> (fmap Just $ c0' .- Pull)
+            <*> (c5 .- Pull) <*> pure Nothing
+            <*> (c10 .- Pull) <*> pure Nothing)
+           (\e () -> do
+              push c0  $ filter0 e
+              push c0' $ filter0 e
+              push c5  $ filter5 e
+              push c10 $ filter10 e)
+           (\_ _ -> return ())
